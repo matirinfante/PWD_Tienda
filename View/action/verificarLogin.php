bb0b98b9cd5389c7b5fbb026c7447a836983a956
Matias <maty.infante@gmail.com>
2021-11-06 14:55:42 -0300

    Login basic

---
+++ View/action/verificarLogin.php
@@ -0,0 +1,12 @@
+<?php
+include_once("../../config.php");
+$datos = data_submitted();
+$session = new Session();
+if ($session->iniciar($datos['usnombre'], $datos['uspass'])) {
+    header('location:../index/index.php');
+    exit();
+} else {
+    header('location:../index/login.php');
+    exit();
+}
+?>
